8f6e1c2bcfc9289782d3fff307d38d70fbe332a7
h-k11sk <k11sk.tagr@gmail.com>
2018-08-17 11:10:19 +0900

    Added Blog app and details

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,13 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
-    body = models.TextField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %y')
