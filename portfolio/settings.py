fdbf1f307a8eb447c724c98d65b393abdad36559
h-k11sk <k11sk.tagr@gmail.com>
2018-08-17 15:32:27 +0900

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,4 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/' 
+STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
